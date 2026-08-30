9e193fc0836210ec272f06db51628a648d1e7316
PeaceDeath04 <d.v.smetanin@rambler.ru>
2026-08-30 23:24:12 +0300

    fix6

--- update_system.sh
+++ update_system.sh
@@ -3,7 +3,7 @@
 source ./utils.sh
 
 apt_update() {
-    run_command apt-get install pv -y
+
     echo "Обновление списка пакетов..."
 
     run_command apt-get update || return 1
@@ -11,17 +11,15 @@ apt_update() {
     echo
     echo "Обновление пакетов..."
 
-    sudo apt-get -y \
-        -o Dpkg::Progress-Fancy="0" \
-        -o Dpkg::Stdout-Filtering="0" \
-        upgrade 2>&1 \
-        | grep --line-buffered -oP 'Progress:\s*\[\s*\K\d+(?=%\])' \
-        | pv -n -s 100 -p -F "Обновление пакетов %p%% [%b]" \
-        > /dev/null
+    apt-get \
+        -y \
+        -o Dpkg::Progress-Fancy="1" \
+        upgrade
 
-    local RESULT=${PIPESTATUS[0]}
+    local RESULT=$?
 
     if [[ "$RESULT" -ne 0 ]]; then
+        echo
         echo "[✗] Ошибка обновления пакетов"
         echo "[✗] Код ошибки: $RESULT"
         return "$RESULT"
