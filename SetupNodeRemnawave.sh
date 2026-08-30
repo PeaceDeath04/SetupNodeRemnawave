151894f76128d89fc61f122b025d14b6e58dcb95
PeaceDeath04 <d.v.smetanin@rambler.ru>
2026-08-31 00:48:16 +0300

    test32

--- SetupNodeRemnawave.sh
+++ SetupNodeRemnawave.sh
@@ -10,9 +10,6 @@ print_step "Проверка системы"
 
 if check_sys; then
     print_success "Система соответствует требованиям"
-else
-    print_error "Проверка системы не пройдена"
-    exit 1
 fi
 
 
@@ -20,9 +17,6 @@ print_step "Обновление системы"
 
 if apt_update; then
     print_success "Система успешно обновлена"
-else
-    print_error "Не удалось обновить систему"
-    exit 1
 fi
 
 
@@ -30,16 +24,4 @@ print_step "Настройка файла подкачки"
 
 if setup_swap; then
     print_success "Swap успешно настроен: ${NEED_SWAP_SIZE}G"
-else
-    print_error "Не удалось настроить Swap"
-    exit 1
 fi
-
-
-print_step "Проверка Swap"
-
-free -h
-swapon --show
-
-
-countdown
